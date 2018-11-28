b22fa17f037e7ab196738bcf77eca7117bf052e2
Scott Bowler <scott.bowler@learningfuze.com>
2018-11-28 15:38:23 -0800

    Completed project setup

---
+++ src/components/home.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default props => (
+    <div>
+        <h1 className="center">Welcome to Movie Quotes</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi corrupti maiores mollitia iusto maxime nemo quis. Deleniti, sunt sit itaque ad quod debitis voluptates nostrum non laborum labore fugiat velit doloribus harum consectetur fuga expedita excepturi eveniet adipisci cum iure dolores repellat vero porro. Delectus dolorem nobis hic laborum distinctio!</p>
+    </div>
+);
